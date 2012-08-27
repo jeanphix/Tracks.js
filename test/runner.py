07451d46a30599217d0f59cba8c96c3e15c09bb3
jean-philippe serafin <serafinjp@gmail.com>
2012-08-27 17:04:24 +0200

    Cleans up runner.py

--- test/runner.py
+++ test/runner.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 import os
-import time
 
 import nose
 
@@ -8,21 +7,20 @@ from ghost import Ghost
 
 
 def test_mocha():
-    g = Ghost(display=True, wait_timeout=20)
-    index = os.path.abspath(os.path.join(os.path.dirname(__file__), 'index.html'))
+    g = Ghost(wait_timeout=20)
+    index = os.path.abspath(os.path.join(os.path.dirname(__file__),
+        'index.html'))
     g.open('file://%s' % index)
     g.wait_for(lambda: g.evaluate('isMochaRunning()')[0], 10)
-    total =  int(g.evaluate('__mocha_runner__.total')[0])
+    total = int(g.evaluate('__mocha_runner__.total')[0])
     n = 0
     while n < total:
-        g.wait_for(lambda: g.evaluate('__mocha_tests__.length > %s' % n)[0], 10)
-
-        title = g.evaluate('__mocha_tests__[%s].title' % n)[0]
+        g.wait_for(lambda: g.evaluate('__mocha_tests__.length > %s' % n)[0],
+            10)
         state = g.evaluate('__mocha_tests__[%s].state' % n)[0]
-        yield check_state, state, title
-
+        yield check_state, state
         n += 1
 
 
-def check_state(state, title):
+def check_state(state):
     assert state == 'passed'
