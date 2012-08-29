132b874fc8494e1fed0035d5ac2fb9de1b1905ea
jean-philippe serafin <serafinjp@gmail.com>
2012-08-29 16:51:24 +0200

    Reorganizes tracks test suites

--- test/suite.js
+++ test/suite.js
@@ -28,6 +28,20 @@ var helpers = (function () {
 
 
 suite('tracks', function () {
+    suite('#fromAverage()', function () {
+        test('should return the right integer position', function () {
+            expect(tracks.fromAverage(80, 100)).to.be(80);
+            expect(tracks.fromAverage(80, 1000)).to.be(800);
+        });
+        test('should return a rounded integer position', function () {
+            expect(tracks.fromAverage(4, 847)).to.be(34);
+        });
+        test('should return a rounded float position when `precision` arg is provided', function () {
+            expect(tracks.fromAverage(4, 847, 1)).to.be(33.9);
+            expect(tracks.fromAverage(4, 847, 2)).to.be(33.88);
+        });
+    });
+
     suite('#humanizeTime()', function () {
         test('should return a 4 digit fomated string', function () {
             expect(tracks.humanizeTime(184)).to.be("03:04");
@@ -46,20 +60,6 @@ suite('tracks', function () {
         test('should return a rounded decimal average for given values when `precision` arg is provided', function () {
             expect(tracks.toAverage(801, 1000, 2)).to.be(80.10);
             expect(tracks.toAverage(12, 1100, 8)).to.be(1.09090909);
-        });
-    });
-
-    suite('#fromAverage()', function () {
-        test('should return the right integer position', function () {
-            expect(tracks.fromAverage(80, 100)).to.be(80);
-            expect(tracks.fromAverage(80, 1000)).to.be(800);
-        });
-        test('should return a rounded integer position', function () {
-            expect(tracks.fromAverage(4, 847)).to.be(34);
-        });
-        test('should return a rounded float position when `precision` arg is provided', function () {
-            expect(tracks.fromAverage(4, 847, 1)).to.be(33.9);
-            expect(tracks.fromAverage(4, 847, 2)).to.be(33.88);
         });
     });
 
